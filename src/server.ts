fb6a908a019eb0e647e5450f64e246ee59578ec3
Alexander Agruso <agrusoalexander@gmail.com>
2022-11-04 23:36:28 -0500

    ejs layouts and cleaning up default styles

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import path from "path";
 
+import ejsLayouts from "express-ejs-layouts";
+
 const server = express();
 
 server.use(express.static(path.resolve(__dirname, "../dist/public/")));
+server.use(ejsLayouts);
 
 server.set("views", path.resolve(__dirname, "../public/views/"));
 server.set("view engine", "ejs");
