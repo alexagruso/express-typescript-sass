e7bb7e68c2bbae72aa37f3a39084c9a651c12b3b
Alexander Agruso <agrusoalexander@gmail.com>
2022-11-04 22:51:41 -0500

    clean up tsconfig

--- public/scripts/cleave.ts
+++
@@ -1,8 +0,0 @@
-import Cleave from "cleave.js";
-
-const input: HTMLElement = document.querySelector("input")!;
-
-new Cleave(input, {
-    delimiters: ["(", ") ", "-"],
-    blocks: [0, 3, 3, 4],
-});
